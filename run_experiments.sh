10f5ffc061705c98c49a9781406b350d3d7e77b3
E. Has <e.has@liacs.leidenuniv.nl>
2026-01-07 16:20:39 +0100

    fix list calling

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,15 +2,15 @@
 models=("deepwalk_crosswalk" "deepwalk_fairwalk" "deepwalk_skip")
 data=("rice" "deezer")
 tasks=("linkpred" "infmax")
-for d in $data;
+for d in "${data[@]}";
 do
     echo $d
-    for m in $models;
+    for m in "${models[@]}";
     do
         echo $m
-        for t in $tasks
+        for t in "${tasks[@]}";
         do
-            uv run xwalk_reprod --cfg "experiments/${t}_${d}_${m}.yml"
+            echo $t
         done
     done
 done
