4d64408e407fa3640220ac634fd03adfa751279b
EllaHas <ellahas02@gmail.com>
2026-01-07 16:05:52 +0100

    fix more syntax

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,10 +2,10 @@
 models=("deepwalk_crosswalk", "deepwalk_fairwalk", "deepwalk_skip")
 data=("rice", "deezer")
 tasks=("linkpred", "infmax")
-for d in data;
+for d in $data;
 do
     echo $d
-    for m in models;
+    for m in $models;
     do
         echo $m
         for t in $tasks
