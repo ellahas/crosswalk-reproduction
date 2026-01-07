6b0563c5329c58d58109014abd29ca82b4afe8d5
EllaHas <ellahas02@gmail.com>
2026-01-07 16:00:50 +0100

    run multiple tasks and data sets after each other

---
+++ run_experiments.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+models=["deepwalk_crosswalk", "deepwalk_fairwalk", "deepwalk_skip"]
+data=["rice", "deezer"]
+tasks=["linkpred", "infmax"]
+for d in data;
+do
+    echo $d
+    for m in models;
+    do
+        echo $m
+        for t in $tasks
+        do
+            uv run xwalk_reprod --cfg "experiments/${t}_${d}_${m}.yml"
+        done
+    done
+done
