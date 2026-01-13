da1b3d4bb2fd7ea939d8d50aad44c789b6cf50c5
E. Has <e.has@liacs.leidenuniv.nl>
2026-01-13 11:51:24 +0100

    run node classification

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 models=("deepwalk_skip" "deepwalk_fairwalk" "deepwalk_crosswalk")
 data=("pokec_gender")
-tasks=("infmax" "linkpred")
+tasks=("nodeclass")
 for d in "${data[@]}";
 do
     echo $d
@@ -11,7 +11,7 @@ do
         for t in "${tasks[@]}";
         do
             echo $t
-            uv run xwalk_reprod --cfg "experiments/${t}_rice_${m}.yml" --opts DATASET $d
+            uv run xwalk_reprod --cfg "experiments/${t}_rice_${m}.yml" --opts DATASET $d NODE_CLASSIFICATION.LABEL_KEY "labels"
         done
     done
 done
