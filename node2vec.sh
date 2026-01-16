4149e329f38bfc835572ffac71163dc314f714af
EllaHas <ellahas02@gmail.com>
2026-01-16 10:07:43 +0100

    run combinations of node2vec params

---
+++ node2vec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+model=node2vec_simple
+data=rice
+task=linkpred
+ps=(0.01 0.5 1 2)
+qs=(0.01 0.5 1 2)
+
+for p in "${ps[@]}";
+    do
+        echo $p
+        for q in "${qs[@]}"
+            do
+                echo $q
+                uv run xwalk_reprod --cfg "experiments/${task}_${data}_${model}.yml" --opts RUNS 1
